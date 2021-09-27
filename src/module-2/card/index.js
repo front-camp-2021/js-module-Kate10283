b6221f86a01df44a36af60f2bcec3a6684739ed4
Meshchan Katerina <77011490+Kate10283@users.noreply.github.com>
2021-09-27 17:49:32 +0300

    Add files via upload

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -21,7 +21,26 @@ export default class Card {
     this.render();
   }
 
-  render () {
-    // ... your logic
+  render() {
+    this.element = document.createElement("div");
+    let id = document.createElement("p");
+    id.innerHTML = this.id;
+    let image = document.createElement("img");
+    image.src = this.images[0];
+    let rate = document.createElement("p");
+    rate.innerHTML = this.rating;
+    let price = document.createElement("p");
+    price.innerHTML = this.price;
+    let title = document.createElement("p");
+    title.innerHTML = this.title;
+    let category = document.createElement("p");
+    category.innerHTML = this.category;
+    let brand = document.createElement("p");
+    brand.innerHTML = this.brand;
+    this.element.append(id, image, rate, price, title, category, brand);
+  }
+
+  destroy() {
+    this.element.remove();
   }
 }
